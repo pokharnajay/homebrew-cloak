7399dac5cbae7a15d3d558ae2c807b761bf8819b
pokharnajay <pokharnajay@gmail.com>
2026-04-08 15:57:07 +0530

    Update sha256 for error handling release

--- Casks/app.rb
+++ Casks/app.rb
@@ -1,6 +1,6 @@
 cask "app" do
   version "0.2.0"
-  sha256 "0684f41c52134b19b989fffffb374012e553933afe0db6e4c9844321402d0c98"
+  sha256 "5761157cc730609766342a58ce5d106da3191c6fa7b555b215e34aa37c96219f"
 
   url "https://github.com/pokharnajay/cloak/releases/download/v#{version}/Cloak-#{version}-arm64.zip"
   name "Cloak"
